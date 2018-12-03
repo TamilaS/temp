6ded24d2f871ba4744c10168cc74c98ecda4a3d0
madim <mmadikk@gmail.com>
2018-12-03 19:00:52 +0600

    Rename var

--- Temp.java
+++ Temp.java
@@ -2,9 +2,9 @@ class Temp {
 	public static void main (String [] args) {
 		int x = 3+4;
 		String a = "24";
-		int aInt = Integer.parseInt(a);
+		int aInteger = Integer.parseInt(a);
 
 		System.out.println(String.valueOf(x));
-		System.out.println(a + " " + String.valueOf(aInt));
+		System.out.println(a + " " + String.valueOf(aInteger));
 	}
 }
