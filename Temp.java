6dbf9f49f15da5540fe0bda15d6dfc0defd90f5e
madim <mmadikk@gmail.com>
2018-12-03 18:58:45 +0600

    fixed errrors and renamed to Temp.java

---
+++ Temp.java
@@ -0,0 +1,10 @@
+class Temp {
+	public static void main (String [] args) {
+		int x = 3+4;
+		String a = "24";
+		int aInt = Integer.parseInt(a);
+
+		System.out.println(String.valueOf(x));
+		System.out.println(a + " " + String.valueOf(aInt));
+	}
+}
--- sta.java
+++
@@ -1,8 +0,0 @@
-public static void main (String [] args) {
-	int x = 3+4;
-	int aInt = Integer.parseInt(a);
-	String a = "24";
-
-	System.out.println(String.valueOf(x));
-	System.out.println(a + String.valueOf(aInt));
-}
