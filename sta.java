c17e7cb1c9bc581e7d0d9b3c7de9d576f2a951a7
madim <mmadikk@gmail.com>
2018-12-03 18:51:55 +0600

    edited sta.java

--- sta.java
+++ sta.java
@@ -1,4 +1,8 @@
 public static void main (String [] args) {
-int x = 3+4;
-System.out.println(x );
+	int x = 3+4;
+	int aInt = Integer.parseInt(a);
+	String a = "24";
+
+	System.out.println(String.valueOf(x));
+	System.out.println(a + String.valueOf(aInt));
 }
