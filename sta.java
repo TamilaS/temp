4493bf0387eb815b51204dfcd065572157b7be14
madim <mmadikk@gmail.com>
2018-12-03 19:00:03 +0600

    Rename var

---
+++ sta.java
@@ -0,0 +1,10 @@
+class Temp {
+	public static void main (String [] args) {
+		int x = 3+4;
+		String a = "24";
+		int aInteger = Integer.parseInt(a);
+
+		System.out.println(String.valueOf(x));
+		System.out.println(a + " " + String.valueOf(aInteger));
+	}
+}
